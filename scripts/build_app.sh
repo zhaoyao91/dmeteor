355c3855d22b7078620573faf3a9bc99f9414ae4
I am not shine <zhaoyao91@163.com>
2016-09-14 17:59:03 +0800

    change echo to comment

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -2,21 +2,21 @@ set -e
 
 BUNDLE_DIR=/tmp/bundle
 
-echo "Install meteor"
+# install meteor
 curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
 
-echo "Build app"
+# build app
 cd $APP_SRC_DIR
 npm install
 meteor build --directory $BUNDLE_DIR
 
 mv $BUNDLE_DIR/bundle $APP_DIR
 
-echo "Install packages"
+# install packages
 cd $APP_DIR/programs/server
 npm install --production
 
-echo "Clean up"
+# clean up
 rm -rf $APP_SRC_DIR
 rm -rf $BUNDLE_DIR
 rm -rf ~/.meteor
