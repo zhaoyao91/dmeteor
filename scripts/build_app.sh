a850dd8b8e98611c04d70f154d2846c007d41172
I am not shine <zhaoyao91@163.com>
2016-10-18 09:21:22 +0800

    add --unsafe-perm to support built as root user

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -7,14 +7,14 @@ curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
 
 # build app
 cd $APP_SRC_DIR
-npm install
+npm install --unsafe-perm
 meteor build --directory $BUNDLE_DIR
 
 mv $BUNDLE_DIR/bundle $APP_DIR
 
 # install packages
 cd $APP_DIR/programs/server
-npm install --production
+npm install --production --unsafe-perm
 
 # clean up
 rm -rf $APP_SRC_DIR
